01f446294915dcdcbf10728ab6fdc12237078747
Tomás Atanasoff <tomyatana@gmail.com>
2025-02-27 23:08:39 -0300

    Progress

--- lua/nvim-gf-fix/init.lua
+++ lua/nvim-gf-fix/init.lua
@@ -1,7 +1,20 @@
 local M = {}
 
 function M.go_to_file()
-    print("hola")
+    ---@type string
+    local line = vim.api.nvim_get_current_line()
+    ---@type string
+    local line_cpy = line
+    local cursor_col = vim.api.nvim_win_get_cursor(0)[2]
+    local l = 0
+    local r = 0
+    repeat
+        l = line_cpy:find("(", 1, true) or -1
+        r = line_cpy:find(")", 1, true) or -1
+        line_cpy = line_cpy:sub(r+1)
+    until cursor_col < r or r == -1
+    line = line:sub(l, r);
+    print(line)
 end
 
 
@@ -12,6 +25,7 @@ function M.setup(opts)
     if(opts.noremap) then
         return
     end
+    vim.keymap.set("n", "gf", M.go_to_file)
 end
 
 return M
